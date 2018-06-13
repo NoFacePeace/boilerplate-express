2bcb9e8084abe5f541615d6093927467a42c7583
cht <736090467@qq.com>
2018-06-13 14:58:56 +0800

    fix: finish

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
-
+require('dotenv').config();
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(function(req, res, next){
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
 // --> 7)  Mount the Logger middleware here
 
@@ -10,21 +16,48 @@ var app = express();
 
 /** 1) Meet the node console. */
 
+console.log('Hello World');
 
 /** 2) A first working Express Server */
 
+// app.get('/', function(req, res) {
+//     res.send('Hello Express');
+// });
 
 /** 3) Serve an HTML file */
 
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
 /** 4) Serve static assets  */
 
+app.use(express.static(__dirname + '/public'));
 
 /** 5) serve JSON on a specific route */
 
+// app.get('/json', function(req, res) {
+//     res.json({
+//         message: "Hello json",
+//     });
+// });
 
 /** 6) Use the .env file to configure the app */
- 
+
+app.get('/json', function(req, res) {
+    console.log(i ++);
+    console.log(process.env.MESSAGE_STYLE);
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        return res.json({
+            message: 'HELLO JSON',
+        });
+    } else {
+        // console.log(process.env);
+        return res.json({
+            message: 'Hello json',
+        });
+    }
+});
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
@@ -32,20 +65,50 @@ var app = express();
 
 /** 8) Chaining middleware. A Time server */
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({
+        time: req.time,
+    });
+});
 
 /** 9)  Get input from client - Route parameters */
 
+app.get('/:word/echo', function(req, res) {
+    res.json({
+        echo: req.params.word,
+    });
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
+app.get('/name', function(req, res) {
+    var firstname = req.query.first;
+    var lastname = req.query.last;
+    res.json({
+        name: `${firstname} ${lastname}`,
+    });
+});
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
 
 /** 12) Get data form POST  */
 
+app.post('/name', function(req, res) {
+    var firstname = req.body.first;
+    var lastname = req.body.last;
+    res.json({
+        name: `${firstname} ${lastname}`,
+    });
+});
 
 
 // This would be part of the basic setup of an Express app
